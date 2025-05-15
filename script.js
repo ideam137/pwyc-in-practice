be592158836fc0c3b7bac6a3dd28a22b3af4dd82
idaem137 <idaem@miamioh.edu>
2025-05-15 15:52:58 -0400

    before3

--- script.js
+++ script.js
@@ -58,50 +58,50 @@ function generateRecommendation(answers) {
     answers.c1misuse === "yes" &&
     answers.c1software === "yes"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you focus on implementing a PWYC monthly membership. Some pros of this program include greater revenue potential and maintained program integrity. Some cons of this program include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain.";
   } else if (
     answers.c1goals === "Ease of access" &&
     answers.c1misuse === "yes" &&
     answers.c1software === "no"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you focus on implementing a PWYC monthly membership. Part of this may include looking into software that would be able to keep up with these PWYC model needs or learning how to rework your current system to allow for multiple pricing systems. Some pros of this program include greater revenue potential and maintained program integrity. Some cons of this program include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain.";
   } else if (
     answers.c1goals === "Ease of access" &&
     answers.c1misuse === "no" &&
     answers.c1software === "yes"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you consider implementing both a PWYC monthly membership and PWYC day passes. Pros of monthly membership include greater revenue potential and maintained program integrity while pros of day passes include ease of implementation into your software and more accessibility for participants. Cons of monthly membership include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain where cons of day passes include not increasing recurring membership and that they are the easiest to take advantage of.";
   } else if (
     answers.c1goals === "Ease of access" &&
     answers.c1misuse === "no" &&
     answers.c1software === "no"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you consider implementing both a PWYC monthly membership and PWYC day passes. Part of this may include looking into software that would be able to keep up with the PWYC monthly membership needs or learning how to rework your current system to allow for multiple pricing systems. Pros of monthly membership include greater revenue potential and maintained program integrity while pros of day passes include ease of implementation into your software and more accessibility for participants. Cons of monthly membership include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain where cons of day passes include not increasing recurring membership and that they are the easiest to take advantage of.";
   } else if (
     answers.c1goals === "Both" &&
     answers.c1misuse === "yes" &&
     answers.c1software === "yes"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you consider implementing both a PWYC monthly membership and PWYC day passes or just PWYC monthly memberships. Pros of monthly membership include greater revenue potential and maintained program integrity while pros of day passes include ease of implementation into your software and more accessibility for participants. Cons of monthly membership include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain where cons of day passes include not increasing recurring membership and that they are the easiest to take advantage of.";
   } else if (
     answers.c1goals === "Both" &&
     answers.c1misuse === "yes" &&
     answers.c1software === "no"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you consider implementing both a PWYC monthly membership and PWYC day passes or just PWYC monthly memberships. Part of this may include looking into software that would be able to keep up with the PWYC monthly membership needs or learning how to rework your current system to allow for multiple pricing systems. Pros of monthly membership include greater revenue potential and maintained program integrity while pros of day passes include ease of implementation into your software and more accessibility for participants. Cons of monthly membership include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain where cons of day passes include not increasing recurring membership and that they are the easiest to take advantage of.";
   } else if (
     answers.c1goals === "Both" &&
     answers.c1misuse === "no" &&
     answers.c1software === "yes"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you consider implementing both a PWYC monthly membership and PWYC day passes. Pros of monthly membership include greater revenue potential and maintained program integrity while pros of day passes include ease of implementation into your software and more accessibility for participants. Cons of monthly membership include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain where cons of day passes include not increasing recurring membership and that they are the easiest to take advantage of.";
   } else if (
     answers.c1goals === "Both" &&
     answers.c1misuse === "no" &&
     answers.c1software === "no"
   ) {
-    return "Consider backend development with Node.js or explore DevOps tools.";
+    return "We would recommend that you consider implementing both a PWYC monthly membership and PWYC day passes. Part of this may include looking into software that would be able to keep up with the PWYC monthly membership needs or learning how to rework your current system to allow for multiple pricing systems. Pros of monthly membership include greater revenue potential and maintained program integrity while pros of day passes include ease of implementation into your software and more accessibility for participants. Cons of monthly membership include the fact that it may be harder to implement into your gym software and requires more staff time to start/maintain where cons of day passes include not increasing recurring membership and that they are the easiest to take advantage of.";
   } else {
-    return "huh";
+    return "What gets returned if none of the given combinations worked properly.";
   }
 }
